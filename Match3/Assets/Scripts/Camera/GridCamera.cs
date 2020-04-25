1b285e71c4421bfd7a651bf4cae2cb07fb61c015
DESKTOP-POGF7SK\Driiade <benoit.constantin@hotmail.com>
2020-04-25 11:02:24 +0200

    -FIX: Camera distance with different window aspect ratio.

--- Match3/Assets/Scripts/Camera/GridCamera.cs
+++ Match3/Assets/Scripts/Camera/GridCamera.cs
@@ -14,8 +14,23 @@ public class GridCamera : MonoBehaviour
         Grid grid = ServiceProvider.GetService<Grid>();
         Vector3 newPosition = grid.ViewPosition + (Vector3)(new Vector2(grid.Size.x, -grid.Size.y) / 2f);
         newPosition -= new Vector3(-0.5f * ((grid.Size.x+1)%2), -0.5f * ((grid.Size.y+1) % 2), 0); //Center on the grid. We use complicate algorithm to handle all cases, but we don't really need that.
-        newPosition += offset;
+
+        Vector3 thalesOffset = offset;
+
+        //How width is the screen in the camera
+        float cameraDistance = (offset.z);
+        float designedWidth = ((cameraDistance) / Mathf.Cos(Mathf.Deg2Rad * Camera.main.fieldOfView)) * (16f / 9f);
+        float currentWidth = ((cameraDistance) / Mathf.Cos(Mathf.Deg2Rad * Camera.main.fieldOfView)) * Camera.main.aspect;
+
+        thalesOffset.z = (offset.z/ (currentWidth/designedWidth));
+        thalesOffset.x = (offset.x * (currentWidth / designedWidth));
+
+        newPosition.z = 0;
+        newPosition += thalesOffset;
+
         this.transform.position = newPosition;
+
+
     }
 
 }
