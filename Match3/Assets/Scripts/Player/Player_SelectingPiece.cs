95b3e8ff6c06261a8877cd10826974c47547d43d
DESKTOP-POGF7SK\Driiade <benoit.constantin@hotmail.com>
2020-04-26 01:04:20 +0200

    -MOD: Polish selection UX (you can now drag a selected piece)

--- Match3/Assets/Scripts/Player/Player_SelectingPiece.cs
+++ Match3/Assets/Scripts/Player/Player_SelectingPiece.cs
@@ -15,6 +15,12 @@ public partial class Player : StatedMono<PlayerStateEnum>
 
         public override PlayerStateEnum CheckForNextState(StatedMono<PlayerStateEnum> statedMono)
         {
+            Player player = statedMono as Player;
+            if (player.frameDataBuffer.Exists<MessageData<Piece>>((x)=>x.message == "PieceTaken"))
+            {
+                return PlayerStateEnum.DRAGGING_PIECE;
+            }
+
             if (foundPiece)
                 return PlayerStateEnum.WAITING_FOR_INPUT;
 
@@ -31,12 +37,24 @@ public partial class Player : StatedMono<PlayerStateEnum>
 
         public override void OnExit(StatedMono<PlayerStateEnum> statedMono)
         {
-            if (selectedPiece)
+            Player player = statedMono as Player;
+            if (selectedPiece && !player.frameDataBuffer.Exists<MessageData<Piece>>((x) => x.message == "PieceTaken"))
                 selectedPiece.Unselect();
         }
 
         public override void OnUpdate(StatedMono<PlayerStateEnum> statedMono)
         {
+            Player player = statedMono as Player;
+
+            if (Input.GetMouseButton(0)) //Drag the piece if player clic again on the same piece
+            {
+                Vector2 position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
+                if(Vector2.SqrMagnitude((Vector2)selectedPiece.ViewPosition - position) < 0.5f)
+                {
+                    player.frameDataBuffer.AddData(new MessageData<Piece>("PieceTaken", selectedPiece)); //Pass the data to the next state
+                }
+            }
+
             if (Input.GetMouseButtonUp(0))
             {
                 Vector2 position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
